c0188ad3b794214ac55640f755168eb97547e7a8
Ornel P. Serpa <ornel.developer@gmail.com>
2026-05-18 19:30:44 -0500

    Primer commit: Avances Iniciales del proyecto

---
+++ src/scrips/app.js
@@ -0,0 +1,12 @@
+// Obtiene la URL actual de la barra de direcciones
+const currentPath = window.location.pathname;
+console.log(currentPath);
+// Busca todos los enlaces del navegador
+const footerLinks = document.querySelectorAll('.footer-link');
+
+footerLinks.forEach(link => {
+  // Si el href del enlace coincide con la ruta actual...
+  if (link.getAttribute('href') === currentPath || link.href === window.location.href) {
+    link.classList.add('active');
+  }
+});
