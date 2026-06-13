4fe8d2eaa2f9d9f9cd2fb49c7b6e05ca67e24b20
Ornel P. Serpa <ornel.developer@gmail.com>
2026-06-13 12:07:07 -0500

    Actualiza la configuración de Astro y mejora el registro del Service Worker en producción, se realizan avancen en la pagina de "contacto".

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 
-
-
-    export default defineConfig({
+export default defineConfig({
   adapter: vercel(),
-  
-
-
-  // Fuerza a que los estilos se incrusten en el HTML (<style>...</style>)
   build: {
-    inlineStylesheets: 'always'
-  }
-
-  // Opcional: le dice a Astro que nunca genere rutas con '/'
-
-    });
+    inlineStylesheets: 'always',
+  },
+});
