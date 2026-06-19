7ecf3cb4fd5c0c8f933d1639e34448d3ac0134b3
Ornel P. Serpa <ornel.developer@gmail.com>
2026-06-19 07:20:21 -0500

    Pueba de estabilidad y funcionamiento de las nuevas funciones de la app, incluyendo la instalación en línea y la barra flotante para PWA.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '1.0.5.8'; // Incrementamos la versión para forzar la limpieza en los celulares
+const CACHE_NAME = '1.0.5.9'; // Incrementamos la versión para forzar la limpieza en los celulares
 //Comentario de version aumentada
 //1606.1.1
 const CORE_ASSETS = [
