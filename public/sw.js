1079026af1a5e16069f523f73f3e50fb8cc714fe
Ornel P. Serpa <ornel.developer@gmail.com>
2026-06-21 10:22:53 -0500

    Actualización de descarga de caché y corrección de título en la página principal.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '1.0.6.0'; // Incrementamos la versión para forzar la limpieza en los celulares
+const CACHE_NAME = '1.0.6.2'; // Incrementamos la versión para forzar la limpieza en los celulares
 //Comentario de version aumentada
 //1606.1.1
 const CORE_ASSETS = [
@@ -22,6 +22,9 @@ const MEDIA_ASSETS = [
     '/rutas/',
     '/nosotros',
     '/nosotros/',
+    '/contacto',
+    '/contacto/',
+
 
     '/blog',
     '/blog/',
@@ -153,7 +156,10 @@ const MEDIA_ASSETS = [
 'imgs/blogs/avistadores.webp',
 'imgs/blogs/bosques.webp',
 'imgs/blogs/mitologiayaves.webp',
-'imgs/blogs/rutas.webp'
+'imgs/blogs/rutas.webp',
+'imgs/contacto/ornel.avif',
+'imgs/contacto/jhonatan.avif',
+
 
 ];
 
